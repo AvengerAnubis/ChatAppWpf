641651d4f4b490793977ec885e6755d928596ff8
AvengerAnubis <avenger.anubis@gmail.com>
2025-10-06 02:29:36 +0500

    Добавлены классы для будущей реализации

---
+++ ChatLib/NetShared/QuicService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatLib.Shared.NetShared
+{
+    public class QuicService
+    {
+        public QuicService()
+        {
+            // Перспективный протокол для чата, более безопасный, работает поверх UDP
+            // Есть шифрование TLS 1.3 (самописные/подлинные сертификаты)
+            // Но для простого чата пока что overkill
+            throw new NotImplementedException();
+        }
+    }
+}
