976983c12dd6413a1bea359204e93fed7d1df3e1
AvengerAnubis <avenger.anubis@gmail.com>
2025-10-06 05:35:40 +0500

    Создание библиотеки хоста

---
+++ ChatLib.Host/HostUdpService.cs
@@ -0,0 +1,14 @@
+﻿using ChatLib.NetShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatLib
+{
+    public class HostUdpService : UdpService
+    {
+        public HostUdpService() : base() { Port = 25500; }
+    }
+}
