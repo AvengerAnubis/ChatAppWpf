300fe9e7f35f92456ec77faa23747db87b7e281d
AvengerAnubis <avenger.anubis@gmail.com>
2025-10-06 05:35:04 +0500

    Реализация класса пакета сообщения

---
+++ ChatLib/ChatProtocolShared/ChatMessagePacket.cs
@@ -0,0 +1,18 @@
+﻿using ChatLib.NetShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatLib.ChatProtocolShared
+{
+    /// <summary>
+    /// Базовый класс сообщения (текст сообщения, без ника и доп. данных)
+    /// </summary>
+    public class ChatMessagePacket : PacketBase
+    {
+        public string Message { get; set; } = string.Empty;
+    }
+}
