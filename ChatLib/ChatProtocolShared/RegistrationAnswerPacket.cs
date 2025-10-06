2b51c08b4976821d50206123c2ca9446af27bab1
AvengerAnubis <avenger.anubis@gmail.com>
2025-10-06 05:37:04 +0500

    Пакеты регистрации клиента

---
+++ ChatLib/ChatProtocolShared/RegistrationAnswerPacket.cs
@@ -0,0 +1,18 @@
+﻿using ChatLib.NetShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatLib.ChatProtocolShared
+{
+    public enum RegistrationStatus
+    {
+        NameExist, Failure, Success 
+    }
+    public class RegistrationAnswerPacket : PacketBase
+    {
+        public RegistrationStatus RegistrationStatus { get; set; }
+    }
+}
---
+++ ChatLib/ChatProtocolShared/RegistrationRequestPacket.cs
@@ -0,0 +1,20 @@
+﻿using ChatLib.NetShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatLib.ChatProtocolShared
+{
+    public enum ChatConnectionMode
+    {
+        PeerToPeer, Host
+    }
+    public class RegistrationRequestPacket : PacketBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool RequestChatHistory { get; set; } = false;
+        public ChatConnectionMode ConnectionMode { get; set; } = ChatConnectionMode.Host;
+    }
+}
